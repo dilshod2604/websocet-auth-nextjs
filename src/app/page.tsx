160f05f42e013ee3d69731247ab99b36ee1336ea
dilshod2604 <dilshodadilbekov456@gmail.com>
2024-08-21 22:09:24 +0600

    Image delited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
           </a>
         </div>
       </div>
