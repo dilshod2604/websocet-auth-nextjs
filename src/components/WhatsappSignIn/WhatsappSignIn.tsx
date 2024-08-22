7ac04100bfc7ce4e439036948277d7ebe8bf346f
dilshod2604 <dilshodadilbekov456@gmail.com>
2024-08-22 17:22:36 +0600

    WhatsApp sign in finished

--- src/components/WhatsappSignIn/WhatsappSignIn.tsx
+++ src/components/WhatsappSignIn/WhatsappSignIn.tsx
@@ -1,19 +1,37 @@
+"use client";
 import React, { FC } from "react";
 import scss from "./WhatsappSignIn.module.scss";
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Form, Input, Select } from "antd";
 import { IoLogoWhatsapp } from "react-icons/io5";
 import { FcGoogle } from "react-icons/fc";
 import { GrGithub } from "react-icons/gr";
-
+import whatsapp from "../../assets/whatsapp-1.png";
+import Image from "next/image";
+import { Option } from "antd/es/mentions";
+const Prefix = (
+  <Form.Item noStyle>
+    <Select className={scss.prefixSelector}>
+      <Option value="996">+996</Option>
+    </Select>
+  </Form.Item>
+);
 const WhatsappSignIn: FC = () => {
   return (
     <div className={scss.WhatsappSignIn}>
       <div className={scss.content}>
-        <Form>
+        <div className={scss.signIn_img}>
+          <Image src={whatsapp} alt="" />
+        </div>
+        <h3 className={scss.sign_in_title}>WhatsApp</h3>
+        <Form className={scss.form}>
           <Form.Item>
-            <InputNumber />
+            <Input addonBefore={Prefix} placeholder="phone" />
           </Form.Item>
-          <Button icon={<IoLogoWhatsapp />} iconPosition="start">
+          <Button
+            icon={<IoLogoWhatsapp />}
+            iconPosition="start"
+            className={scss.form_button}
+          >
             Sign in
           </Button>
         </Form>
---
+++ src/components/providers/SessionProvider.tsx
@@ -0,0 +1,13 @@
+// "use client"
+// import React, { ReactNode } from 'react';
+// interface SessionProviderProps{
+//     children:ReactNode,
+//     session:
+// }
+// const SessionProvider = ({children,session}) => {
+//     return (
+
+//     );
+// };
+
+// export default SessionProvider;
